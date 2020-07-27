a08e27c65e912469452edda04e1ab90aa037e4f5
Shahram Anver <shahram@datacue.co>
2020-07-28 02:53:41 +0800

    added better logging

--- taint-nodes.sh
+++ taint-nodes.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Checking if we have new nodes without a taint"
 NODES_TO_TAINT=$(kubectl get nodes -l nodetype=redis -o custom-columns=NAME:.metadata.name,TAINTS:.spec.taints --no-headers | grep none | wc -l | awk '{print $1}')
+echo "$NODES_TO_TAIN require tainting"
 if [ NODES_TO_TAINT=="0" ]; then
-    echo "No nodes require tainting"
     exit
 fi
 kubectl taint nodes -l nodetype=redis app=redis:NoSchedule
